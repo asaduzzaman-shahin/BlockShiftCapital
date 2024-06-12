c48c901e7f46504ebcb6e92c5267a2bd42a714c0
Asaduzzaman Shahin <77843436+asaduzzaman-shahin@users.noreply.github.com>
2024-06-13 04:20:50 +0600

    Initial commit

---
+++ js/program.js
@@ -0,0 +1,36 @@
+const navIcon = document.querySelector(".navIcon i");
+const navIcons = document.querySelector(".navIcon");
+const navMobile = document.querySelector(".navMenu ul");
+
+const docu = document.querySelector(".docu");
+
+const menuShow = () => {
+    navMobile.classList.toggle("menuHide");
+    navIcon.classList.toggle("fa-bars");
+    navIcon.classList.toggle("fa-times");
+    navIcons.classList.toggle("shadowChange")
+    
+}
+
+navIcon.addEventListener("click", menuShow);
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".menu ul li a");
+
+window.onscroll=()=>{
+    sections.forEach(sec=>{
+        let top = window.scrollY;
+        let offset =sec.offsetTop- 170;;
+        let height =sec.offsetHeight;
+        let id = sec.getAttribute("id");
+        console.log("top="+ top+"!!!!!!"+"offset="+ offset+"!!!!!!"+"height="+ height+"!!!!!!"+"id="+ id+"!!!!!!");
+
+        if(top >=offset && top<offset + height){
+            navLinks.forEach(links=>{
+                links.classList.remove("active");
+                document.querySelector('ul li a[href*=' + id + ']').classList.add("active");
+                // document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            })
+        }
+    })
+}
